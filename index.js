ada58f3937c5dc6f4f0b05e92187b89e962131c6
snitcoding <giatinhuynh2612@gmail.com>
2023-09-05 17:08:32 +0700

    installed ejs for creating views

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ mongoose.connect('mongodb+srv://s3962053:webproga2@cluster0.qloy7im.mongodb.net/
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch(err => console.log('Error: ', err.message));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
+app.get('/', (req, res) => {
+  res.send('Test test test');
+});
+
 app.use(express.static('public'));
 
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
